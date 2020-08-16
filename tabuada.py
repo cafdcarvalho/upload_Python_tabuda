766d5e099201b939400e4bb02c8a976dff23941a
Carlos Felipe <69130895+cafdcarvalho@users.noreply.github.com>
2020-08-16 20:59:39 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,10 @@
+#Tabuada de Multiplicação ( de 1 a 10)
+num = int(input('Tabuada de : '))
+# num = int(input("Display multiplicação
+for i in range(1, 11):
+          print(num, 'x', i, '=', num*i)
+
+num = int(input('Tabuada de : '))
+# num = int(input("Display multiplicação
+for i in range(1, 11):
+          print(num, 'x', i, '=', num*i)
